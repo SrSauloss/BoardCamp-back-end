ec9aec189f4fc0b14079f152d1b4552304e18b85
Bruna Topan <brutopanb@gmail.com>
2021-10-22 09:36:02 -0300

    Preparando review

--- index.js
+++
@@ -1,7 +0,0 @@
-import routes from "./src/Routes/index.js";
-import app from "./src/app.js";
-
-routes(app);
-
-app.listen(4000);                                                                                                                                                                                                                                                                           
-console.log("Server on");                                                                                                                                                                                                                                                                                                                                     
